6bc2bc8e5feef78630a913c2868ecd0d4b54e395
Ismail0v12 <orifisamilov12@mail.ru>
2023-08-24 13:49:19 +0500

    new

--- bot/main.py
+++ bot/main.py
@@ -25,29 +25,28 @@ async def course(message: types.Message):
     await message.answer(f"Here's the course information.", reply_markup=inline_category(category_list))
 
 
-# @dp.message_handler(text="video")
-# async def course(message: types.Message):
-#     # category_list = get_category()
-#     await bot.send_video(chat_id=message.chat.id, video='BAACAgIAAxkBAAIBFmTkujhQrB9O909KniuACSmIBFMYAALtMwACtLooS0-VhdZZWMsWMAQ')
-#     # await message.answer(f"Here's the course information.", reply_markup=inline_category(category_list))
-
-
 @dp.callback_query_handler(lambda c: c.data.startswith("category_"))
 async def course_category_parent(callback_query: types.CallbackQuery):
     category_id = callback_query.data.split('_')[1]
     category_parent = get_category_parent(category_id)
-    await callback_query.message.answer(text="Course", reply_markup=inline_category_parent(category_parent))
-    await callback_query.answer(cache_time=4)
+    if category_parent:
+        await callback_query.message.answer(text="Course", reply_markup=inline_category_parent(category_parent))
+        await callback_query.answer(cache_time=4)
+    else:
+        await callback_query.message.answer(text="Hozircha Bu yonalish boyicha kurs qoshilmadi")
 
 
 @dp.callback_query_handler(lambda c: c.data.startswith("parent_"))
 async def course_video(callback_query: types.CallbackQuery):
     course_id = callback_query.data.split('_')[1]
     data = get_course_video(course_id)
-    for i in data:
-        with open(f"../media/{i}", 'rb') as video:
-            print(video)
-            await callback_query.bot.send_video(chat_id=callback_query.message.chat.id, video=video)
+    if data:
+        for i in data:
+            with open(f"../media/{i}", 'rb') as video:
+                print(video)
+                await callback_query.message.answer(text="Video Dowland")
+                await callback_query.bot.send_video(chat_id=callback_query.message.chat.id, video=video)
+    await callback_query.message.answer(text="Not Video")
 
 
 @dp.message_handler(commands=['help'])
